64b1e0734d14c8b1ea9b72194ee01187daa44903
Appu2804 <goparna.nasina@psiog.com>
2023-02-14 12:41:35 +0530

    Created a package "TurketSC" Created a section "UsrGoparnaSection"

---
+++ Schemas/UsrGoparnaSection1Page/UsrGoparnaSection1Page.js
@@ -0,0 +1,100 @@
+define("UsrGoparnaSection1Page", [], function() {
+	return {
+		entitySchemaName: "UsrGoparnaSection",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{
+			"Files": {
+				"schemaName": "FileDetailV2",
+				"entitySchemaName": "UsrGoparnaSectionFile",
+				"filter": {
+					"masterColumn": "Id",
+					"detailColumn": "UsrGoparnaSection"
+				}
+			}
+		}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName35639ca0-e46a-4b56-9e81-d0fbb5ef7a0c",
+				"values": {
+					"layout": {
+						"colSpan": 24,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "ProfileContainer"
+					},
+					"bindTo": "UsrName"
+				},
+				"parentName": "ProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"parentName": "Tabs",
+				"propertyName": "tabs",
+				"index": 0,
+				"name": "NotesAndFilesTab",
+				"values": {
+					"caption": {
+						"bindTo": "Resources.Strings.NotesAndFilesTabCaption"
+					},
+					"items": []
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"name": "Files",
+				"values": {
+					"itemType": Terrasoft.ViewItemType.DETAIL
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesAndFilesTab",
+				"propertyName": "items",
+				"name": "NotesControlGroup",
+				"values": {
+					"itemType": Terrasoft.ViewItemType.CONTROL_GROUP,
+					"caption": {
+						"bindTo": "Resources.Strings.NotesGroupCaption"
+					},
+					"items": []
+				}
+			},
+			{
+				"operation": "insert",
+				"parentName": "NotesControlGroup",
+				"propertyName": "items",
+				"name": "Notes",
+				"values": {
+					"bindTo": "UsrNotes",
+					"dataValueType": Terrasoft.DataValueType.TEXT,
+					"contentType": Terrasoft.ContentType.RICH_TEXT,
+					"layout": {
+						"column": 0,
+						"row": 0,
+						"colSpan": 24
+					},
+					"labelConfig": {
+						"visible": false
+					},
+					"controlConfig": {
+						"imageLoaded": {
+							"bindTo": "insertImagesToNotes"
+						},
+						"images": {
+							"bindTo": "NotesImagesCollection"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
---
+++ Schemas/UsrGoparnaSectiona4f707b7Section/UsrGoparnaSectiona4f707b7Section.js
@@ -0,0 +1,8 @@
+define("UsrGoparnaSectiona4f707b7Section", [], function() {
+	return {
+		entitySchemaName: "UsrGoparnaSection",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
